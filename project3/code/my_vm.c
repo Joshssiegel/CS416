70dc685deca5d93e3373baa9781f390992afba10
adverma98 <av558@scarletmail.rutgers.edu>
2019-03-16 12:21:05 -0400

    early commits

--- project3/code/my_vm.c
+++ project3/code/my_vm.c
@@ -55,10 +55,6 @@ void get_value(void *va, void *val, int size) {
 void mat_mult(void *mat1, void *mat2, int size, void *answer) {
     //given two arrays of length: size * size
     //multiply them as matrices and store the computed result in answer
-<<<<<<< HEAD
-
-=======
     set_physical_mem();
->>>>>>> 0525e0f4e0f4da03377df16716742491b0b50976
    //Hint: You will do indexing as [i * size + j] where i, j are the indices of matrix being accessed
 }
