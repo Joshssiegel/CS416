5fc6414e5a3986342b23b424740fddce85a75ac4
Joshssiegel <jss393@ls.cs.rutgers.edu>
2019-04-14 20:50:44 -0400

    Added shell scripts

---
+++ project4/code/mount.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+ ./tfs -s /tmp/jss393/mountdir
+ echo Mounted /tmp/jss393/mountdir
+
---
+++ project4/code/unmount.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+fusermount -u /tmp/jss393/mountdir 
+ echo unmounted /tmp/jss393/mountdir
+
